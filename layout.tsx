ee64170aabea7288918daf47462d662c15a87456
masumishiohara <github@masumishiohara.com>
2026-05-16 01:10:09 +0900

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,27 +1,13 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import './globals.css'
 
-:root {
-  background: #030303;
-  color: #f7f3ed;
-}
-
-html {
-  scroll-behavior: smooth;
-}
-
-body {
-  margin: 0;
-  background: #030303;
-}
-
-img {
-  -webkit-user-drag: none;
-  user-select: none;
-}
-
-::selection {
-  background: #f7f3ed;
-  color: #030303;
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  )
 }
