1792214258686b38c62b78cb48e20b2830ff2aa9
masumishiohara <github@masumishiohara.com>
2026-05-20 01:41:20 +0900

    Update page.tsx

--- app/projects/portrait-of-fruits/page.tsx
+++ app/projects/portrait-of-fruits/page.tsx
@@ -1,6 +1,6 @@
 export default function PortraitOfFruitsPage() {
   return (
-    <main className="museumProjectPage">
+    <main className="museumProjectPage room-pf">
       <section className="museumProjectHero">
 
         <div className="museumCollectionHeader">
