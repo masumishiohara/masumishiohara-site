69105bca9ba0e4f635cd2560d64fec324a9f8950
masumishiohara <github@masumishiohara.com>
2026-05-20 19:29:00 +0900

    Update page.tsx

--- app/exhibitions/page.tsx
+++ app/exhibitions/page.tsx
@@ -3,36 +3,72 @@ const exhibitions = [
     year: "2026",
     title: "Karuizawa Photo Fest",
     type: "Exhibition",
+    note:
+      "Installation of cultivated botanical works and archival studies.",
   },
 
   {
     year: "2025",
     title: "Photoville",
     type: "Exhibition",
+    note:
+      "Botanical portrait installation and public presentation.",
   },
 
   {
     year: "2025",
     title: "Arte Laguna Prize",
     type: "Exhibition",
+    note:
+      "Cultivated form and object-oriented botanical studies.",
   },
 
   {
     year: "2025",
     title: "KEW Gardens",
     type: "Outdoor Installation",
+    note:
+      "Botanical installation research within institutional garden space.",
   },
 
   {
     year: "2025",
     title: "Fujingaho",
     type: "Publication",
+    note:
+      "Editorial feature on cultivation and botanical archive practice.",
   },
 ];
 
 export default function ExhibitionsPage() {
   return (
-    <main className="museumProjectPage">
+    <main className="museumProjectPage room-exhibitions">
+
+      <div className="museumInstitutionBar">
+
+        <div className="museumInstitutionBarLeft">
+          <span>
+            MAISON BOTANIQUE ARCHIVE
+          </span>
+
+          <span className="museumInstitutionDot" />
+
+          <span>
+            EXHIBITIONS
+          </span>
+
+          <span className="museumInstitutionDot" />
+
+          <span>
+            MBA-EX
+          </span>
+        </div>
+
+        <div className="museumInstitutionBarRight">
+          Institutional Activity
+        </div>
+
+      </div>
 
       <section className="museumProjectHero">
 
@@ -79,8 +115,12 @@ export default function ExhibitionsPage() {
                 {item.title}
               </h2>
 
-              <p>
+              <p className="museumExhibitionType">
                 {item.type}
+              </p>
+
+              <p className="museumExhibitionNote">
+                {item.note}
               </p>
 
             </div>
