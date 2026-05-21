0a1f466a9e3f8a96510a7e0a96c152b88869ce4a
masumishiohara <github@masumishiohara.com>
2026-05-21 19:57:29 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,83 +1,37 @@
 export default function HomePage() {
   return (
     <main className="simpleWorksPage">
-
       <section className="simpleWorksHero">
-
-        <p className="simpleArtistName">
-          MASUMI SHIOHARA
-        </p>
+        <p className="simpleArtistName">MASUMI SHIOHARA</p>
 
         <h1>
-          Fruit,
-          <br />
           Cultivated
           <br />
-          as Form
+          Fruit Works
         </h1>
 
         <p className="simpleArtistText">
-  Fruit grower, breeder, and artist based in Japan.
-</p>
-
+          Fruit grower, breeder, and artist based in Japan.
+        </p>
       </section>
 
       <section className="simpleWorksGrid">
-
-        <a
-          href="/projects/botanical-portraits"
-          className="heroWorkCard"
-        >
-
-          <img
-            src="/botanical.jpg"
-            alt="Fruit artwork by Masumi Shiohara"
-          />
+        <a href="/projects/botanical-portraits" className="heroWorkCard">
+          <img src="/botanical.jpg" alt="Fruit artwork by Masumi Shiohara" />
 
           <div className="heroWorkOverlay">
-
-            <span>
-              MASUMI SHIOHARA
-            </span>
-
-            <h2>
-              Fruit Portraits
-            </h2>
-
-            <div className="heroWorkSignature">
-
-              Orchard Time /
-              Cultivated Form /
-              Pomological Archive
-
-            </div>
-
+            <span>MASUMI SHIOHARA</span>
+            <h2>Fruit Portraits</h2>
           </div>
-
         </a>
-
       </section>
 
       <section className="simpleFooterLinks">
-
-        <a href="/projects/botanical-portraits">
-          Works
-        </a>
-
-        <a href="/about">
-          About
-        </a>
-
-        <a href="/exhibitions">
-          Exhibitions
-        </a>
-
-        <a href="/contact">
-          Contact
-        </a>
-
+        <a href="/projects/botanical-portraits">Works</a>
+        <a href="/about">About</a>
+        <a href="/exhibitions">Exhibitions</a>
+        <a href="/contact">Contact</a>
       </section>
-
     </main>
   );
 }
